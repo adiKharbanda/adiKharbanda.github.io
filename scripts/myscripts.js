811a3baf92c1275ab9b78cdb7768af6a7d7e046c
Aditya Kharbanda <aditya.kharbanda@theinternational.org>
2015-02-07 01:42:43 -0500

    Notification+Img+Footer change
    Changing to new version

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -10,17 +10,20 @@ $(document).ready(function(){
   	if(count%2==0){
 	$(".head").css("margin-left","3%");
 	$(".conversation").css("opacity","0");
+
 	
   	}
 
   	else{
-	$(".head").css("margin-left","17%");
-  	$(".head").css("transition", "all 140ms ease-out");
-  	$(".head").css("transition-duration","450ms");
+	$(".head").css("margin-left","200px");
+  	$(".head").css("transition", "all 440ms ease-out");
+  	$(".head").css("transition-duration","350ms");
   	$(".conversation").css("opacity","1");
   	$(".conversation").css("transition", "all 280ms ease-out");
-  
-
+    $(".conversation").css("transition-timing-function", "cubic-bezier(0.135,1.225,0.21,1.09");
+    $(".conversation").css("transition-delay","220ms");
+    $(".conversation").css("transition-duration","280ms") ;
+    $(".testadi").css("opacity","0");
 
   	}
   
@@ -46,14 +49,7 @@ $(".circle").mouseout(function(){
   });
 
 
-$('.fdw-background').hover(
-    function () {
-      $(this).animate({opacity:'1'});
-    },
-    function () {
-      $(this).animate({opacity:'0'});
-    }
-  );  
+ 
 });
  
 
